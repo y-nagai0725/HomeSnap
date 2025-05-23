f43155ee920a65b5844ca9be75485f78ed981c92
y-nagai0725 <y.nagai0725@gmail.com>
2025-05-23 21:00:01 +0900

    jsのコメント追加途中

--- js/script.js
+++ js/script.js
@@ -1,66 +1,109 @@
+/**
+ * historyアイテムクラス
+ */
 class HistoryItem {
-  //表示切替用：duration
+  /**
+   * 共通duration(秒)
+   */
   duration = 0.4;
 
-  //
-  easing = gsapEasing;
-
-  //表示切替用：y方向への移動量
-  yMove = "40px";
+  /**
+   * 画像アニメーションduration(秒)
+   */
+  imageAnimationDuration = 0.3;
 
   /**
-   *
+   * 共通easing
+   */
+  easing = gsapEasing;
+
+  /**
+   * 画像アニメーションeasing
+   */
+  imageAnimationEasing = "back.out(1.4)";
+
+  /**
+   * y方向の初期値(px)
+   */
+  yMove = 40;
+
+  /**
+   * 序数
    */
   number;
 
   /**
-   *
+   * 序数アニメーション
    */
   currentNumberAnimation;
 
   /**
-   *
+   * 説明文
    */
   description;
 
   /**
-   *
+   * 説明文アニメーション
    */
   currentDescriptionAnimation;
 
   /**
-   *
+   * 画像
    */
   image;
 
   /**
-   *
+   * 画像アニメーション
    */
   currentImageAnimation;
 
+  /**
+   * タイトル
+   */
   title;
 
+  /**
+   * 年
+   */
   year;
 
-  progressActiveBar = document.querySelector(".history__progress-active-bar");
-
-  progressYearWrapper = document.querySelector(".history__progress-year-wrapper");
+  /**
+   * index
+   */
+  index;
 
   /**
+   * indexが偶数番目かどうか
+   */
+  isEven;
+
+  /**
+   * 開始deg
+   */
+  startRotate;
+
+  /**
+   * 終了deg
+   */
+  endRotate;
+
+  /**
+   * コンストラクタ
    *
-   * @param {*} e
+   * @param {*} e historyアイテム要素(.history__item)
+   * @param {*} index index
    */
   constructor(e, index) {
     this.number = e.querySelector(".history__number-wrapper");
     this.description = e.querySelector(".history__description-wrapper");
     this.image = e.querySelector(".history__image-wrapper");
+    this.index = index;
     this.isEven = index % 2 !== 0;
     this.startRotate = this.isEven ? "-8deg" : "8deg";
     this.endRotate = this.isEven ? "8deg" : "-8deg";
-    this.index = index;
 
-    //sp,tab表示での初期化
-    mm.add("(max-width: 1023px)", () => {
+    //序数と説明文のsp,tab表示での初期化
+    mm.add(`(max-width: ${pcBreakPoint - 1}px)`, () => {
       gsap.set(this.number, {
         autoAlpha: 0,
         y: this.yMove,
@@ -73,8 +116,8 @@ class HistoryItem {
       });
     });
 
-    //pc表示での初期化
-    mm.add("(min-width: 1024px)", () => {
+    //序数と説明文のpc表示での初期化
+    mm.add(`(min-width: ${pcBreakPoint}px)`, () => {
       gsap.set(this.number, {
         autoAlpha: 0,
         y: this.yMove,
@@ -87,13 +130,14 @@ class HistoryItem {
       });
     });
 
+    //画像の初期化
     gsap.set(this.image, {
       autoAlpha: 0,
       "--image-rotate": this.startRotate,
       "--peseudo-rotate": this.endRotate,
     });
 
-    //タイトル作成、追加、初期化
+    //タイトル要素作成、追加、初期化
     this.title = document.createElement("span");
     this.title.className = "history__progress-title";
     this.title.textContent = e.querySelector(".history__item-title").textContent;
@@ -102,7 +146,7 @@ class HistoryItem {
       autoAlpha: 0,
     });
 
-    //year
+    //年要素作成、追加
     this.year = document.createElement("span");
     this.year.className = "history__progress-year";
     this.year.textContent = e.querySelector(".history__item-year").textContent;
@@ -110,7 +154,9 @@ class HistoryItem {
     document.querySelector(".history__progress-bar").before(this.year);
   }
 
-  //アイテム表示
+  /**
+   * アイテム表示
+   */
   show() {
     this.showNumber();
     this.showDescription();
@@ -120,7 +166,9 @@ class HistoryItem {
     this.updateProgressBar();
   }
 
-  //アイテム非表示
+  /**
+   * アイテム非表示
+   */
   hide() {
     this.hideNumber();
     this.hideDescription();
@@ -129,24 +177,31 @@ class HistoryItem {
     this.inactiveYear()
   }
 
+  /**
+   * プログレスバー更新
+   */
   updateProgressBar() {
+    const progressYearWrapper = document.querySelector(".history__progress-year-wrapper");
+    const progressActiveBar = document.querySelector(".history__progress-active-bar");
     const startPercent = (100 / historyItems.length / 2);
     const plusPercent = (100 / historyItems.length) * this.index;
 
-    mm.add("(max-width: 1023px)", () => {
-      gsap.timeline().to(this.progressActiveBar, {
+    //sp,tab表示
+    mm.add(`(max-width: ${pcBreakPoint - 1}px)`, () => {
+      gsap.timeline().to(progressActiveBar, {
         duration: this.duration,
         ease: this.easing,
         width: (startPercent + plusPercent) + "%",
-      }).to(this.progressYearWrapper, {
+      }).to(progressYearWrapper, {
         duration: this.duration,
         ease: this.easing,
         xPercent: (-(100 / historyItems.length * 0.8) * this.index),
       }, "<");
     });
 
-    mm.add("(min-width: 1024px)", () => {
-      gsap.timeline().to(this.progressActiveBar, {
+    //pc表示
+    mm.add(`(min-width: ${pcBreakPoint}px)`, () => {
+      gsap.timeline().to(progressActiveBar, {
         duration: this.duration,
         ease: this.easing,
         width: (startPercent + plusPercent) + "%",
@@ -154,14 +209,23 @@ class HistoryItem {
     });
   }
 
+  /**
+   * 年要素にクラス付与
+   */
   activeYear() {
     this.year.classList.add("js-large");
   }
 
+  /**
+   * 年要素からクラス削除
+   */
   inactiveYear() {
     this.year.classList.remove("js-large");
   }
 
+  /**
+   * タイトル表示
+   */
   showTitle() {
     if (this.currentTitleAnimation) {
       this.currentTitleAnimation.kill();
@@ -173,6 +237,9 @@ class HistoryItem {
     });
   }
 
+  /**
+   * タイトル非表示
+   */
   hideTitle() {
     if (this.currentTitleAnimation) {
       this.currentTitleAnimation.kill();
@@ -184,28 +251,32 @@ class HistoryItem {
     });
   }
 
-  //画像表示
+  /**
+   * 画像表示
+   */
   showImage() {
     if (this.currentImageAnimation) {
       this.currentImageAnimation.kill();
     }
     this.currentImageAnimation = gsap.to(this.image, {
-      duration: this.duration,
-      ease: this.easing,
+      duration: this.imageAnimationDuration,
+      ease: this.imageAnimationEasing,
       autoAlpha: 1,
       "--image-rotate": this.endRotate,
       "--peseudo-rotate": this.startRotate,
     });
   }
 
-  //画像非表示
+  /**
+   * 画像非表示
+   */
   hideImage() {
     if (this.currentImageAnimation) {
       this.currentImageAnimation.kill();
     }
     this.currentImageAnimation = gsap.to(this.image, {
-      duration: this.duration,
-      ease: this.easing,
+      duration: this.imageAnimationDuration,
+      ease: this.imageAnimationEasing,
       autoAlpha: 0,
       "--image-rotate": this.startRotate,
       "--peseudo-rotate": this.endRotate,
@@ -213,7 +284,7 @@ class HistoryItem {
   }
 
   /**
-   *
+   * 序数表示
    */
   showNumber() {
     if (this.currentNumberAnimation) {
@@ -228,7 +299,7 @@ class HistoryItem {
   }
 
   /**
-   *
+   * 序数非表示
    */
   hideNumber() {
     if (this.currentNumberAnimation) {
@@ -241,7 +312,9 @@ class HistoryItem {
     });
   }
 
-  //説明テキスト表示
+  /**
+   * 説明文表示
+   */
   showDescription() {
     if (this.currentDescriptionAnimation) {
       this.currentDescriptionAnimation.kill();
@@ -254,7 +327,9 @@ class HistoryItem {
     });
   }
 
-  //説明テキスト非表示
+  /**
+   * 説明文非表示
+   */
   hideDescription() {
     if (this.currentDescriptionAnimation) {
       this.currentDescriptionAnimation.kill();
@@ -287,52 +362,41 @@ const pcBreakPoint = 1024;
  */
 const tabBreakPoint = 768;
 
-const fv = document.querySelector(".fv");
-
-const fvYear = document.querySelector(".fv__year-wrapper");
-
-const fvText = document.querySelector(".fv__text");
-
-const fvHeaderLogo = document.querySelector(".header__logo");
-
-const fvAnniversaryLogo = document.querySelector(".fv__anniversary-logo");
-
-const fvScrollLine = document.querySelector(".fv__scroll-line");
-
-const fvMessage = document.querySelector(".fv__message");
-
-const fvCircleTextGray = document.querySelector(".fv__circle-text-gray");
-
-const fvSmallCircleWrapper = document.querySelector(".fv__small-circle-wrapper");
-
-const fvBgSvg = document.querySelector(".fv__bg-svg");
-const svgPath_1 = document.querySelector(".fv__bg-svg-path-1");
-const svgPath_2 = document.querySelector(".fv__bg-svg-path-2");
-const svgPath_3 = document.querySelector(".fv__bg-svg-path-3");
-const svgPath_4 = document.querySelector(".fv__bg-svg-path-4");
-
-const fvImageWrapper = document.querySelector(".fv__image-wrapper");
-
+/**
+ *
+ */
 const beforePcBgSvgPath_1 = "M1912.83 -69.6496C2085.01 175.95 2095.69 868.6 1874.32 912.63C1652.94 956.65 1484.7 797.42 1511.26 452.34C1537.82 107.27 1141.73 173.86 1185.59 -69.6496C1229.44 -313.17 1740.64 -315.26 1912.83 -69.6496Z";
 const beforePcBgSvgPath_2 = "M434.598 756.65C408.033 1101.73 804.12 1035.14 760.262 1278.65C716.413 1522.17 205.207 1524.26 33.0224 1278.65C-139.142 1033.05 -149.827 340.39 71.5435 296.37C292.914 252.34 461.163 411.58 434.598 756.65Z";
 const beforePcBgSvgPath_3 = "M768.273 -432C966.881 -341.87 1038.71 31.2002 574.164 197.68C109.631 364.16 286.729 779.58 -104.283 774.43C-495.174 769.27 -492.924 108.1 -220.25 -162.85C52.4243 -433.81 569.674 -522.13 768.273 -432Z";
 const beforePcBgSvgPath_4 = "M1174.48 1005.04C1639.02 838.57 1461.92 423.14 1852.95 428.29C2243.84 433.45 2241.59 1094.48 1968.9 1365.58C1696.23 1636.54 1178.98 1724.87 980.37 1634.73C781.772 1544.6 709.946 1171.52 1174.48 1005.04Z";
 
+/**
+ *
+ */
 const beforeSpBgSvgPath_1 = "M292.56 -79.3898C391.77 -30.5798 613.89 219.04 518.24 267.68C422.6 316.33 305.9 321.99 252.58 205.7C199.27 89.4102 88.97 136.38 59.43 49.8202C29.89 -36.7298 193.34 -128.22 292.56 -79.3898Z";
 const beforeSpBgSvgPath_2 = "M153.59 505.08C206.93 621.42 317.26 574.42 346.83 661.04C376.4 747.65 212.86 839.15 113.59 790.31C14.35 741.47 -207.86 491.74 -112.17 443.07C-16.47 394.39 100.25 388.74 153.59 505.08Z";
 const beforeSpBgSvgPath_3 = "M-77.44 -42.8499C18.66 -118.15 184.42 -126.19 243.25 -90.0099C302.08 -53.8299 310.12 67.0301 157.87 101.87C5.61996 136.71 45.04 275.01 -77.44 258.32C-199.93 241.66 -173.52 32.4401 -77.44 -42.8499Z";
 const beforeSpBgSvgPath_4 = "M224.86 577.14C377.11 542.3 337.69 404 460.18 420.69C582.67 437.38 556.29 646.6 460.18 721.9C364.07 797.2 198.31 805.24 139.48 769.03C80.65 732.82 72.61 611.98 224.86 577.14Z";
 
+/**
+ *
+ */
 const afterPcBgSvgPath_1 = "M1405 540C1405 785.767 1205.77 985 960 985C714.233 985 516 785.767 515 540C514 294.233 714.233 95 960 95C1205.77 95 1405 294.233 1405 540Z";
 const afterPcBgSvgPath_2 = "M1405 540C1405 785.767 1205.77 985 960 985C714.233 985 516 785.767 515 540C514 294.233 714.233 95 960 95C1205.77 95 1405 294.233 1405 540Z";
 const afterPcBgSvgPath_3 = "M731 172C731 249.32 668.32 312 591 312C513.68 312 452 249.32 451 172C451 94.6801 513.68 32 591 32C668.32 32 731 94.6801 731 172Z";
 const afterPcBgSvgPath_4 = "M731 172C731 249.32 668.32 312 591 312C513.68 312 452 249.32 451 172C451 94.6801 513.68 32 591 32C668.32 32 731 94.6801 731 172Z";
 
+/**
+ *
+ */
 const afterSpBgSvgPath_1 = "M447 333C446 476.594 330.594 593 187 593C43.406 593 -73 476.594 -73 333C-73 189.406 43.406 73 187 73C330.594 73 447 189.406 447 333Z";
 const afterSpBgSvgPath_2 = "M447 333C446 476.594 330.594 593 187 593C43.406 593 -73 476.594 -73 333C-73 189.406 43.406 73 187 73C330.594 73 447 189.406 447 333Z";
 const afterSpBgSvgPath_3 = "M184 73C183 104.48 158.48 130 127 130C95.5198 130 70 104.48 70 73C70 41.5198 95.5198 16 127 16C158.48 16 184 41.5198 184 73Z";
 const afterSpBgSvgPath_4 = "M184 73C183 104.48 158.48 130 127 130C95.5198 130 70 104.48 70 73C70 41.5198 95.5198 16 127 16C158.48 16 184 41.5198 184 73Z";
 
+/**
+ *
+ */
 const bgSvgPathList = {
   "before": { "pc": [beforePcBgSvgPath_1, beforePcBgSvgPath_2, beforePcBgSvgPath_3, beforePcBgSvgPath_4], "sp": [beforeSpBgSvgPath_1, beforeSpBgSvgPath_2, beforeSpBgSvgPath_3, beforeSpBgSvgPath_4] },
   "after": { "pc": [afterPcBgSvgPath_1, afterPcBgSvgPath_2, afterPcBgSvgPath_3, afterPcBgSvgPath_4], "sp": [afterSpBgSvgPath_1, afterSpBgSvgPath_2, afterSpBgSvgPath_3, afterSpBgSvgPath_4] },
@@ -379,6 +443,20 @@ let contentsSlideAnimation;
  * @param {Number} windowWidth ウィンドウ幅
  */
 function setSvgAnimation(windowWidth) {
+  const fv = document.querySelector(".fv");
+  const fvYear = document.querySelector(".fv__year-wrapper");
+  const fvText = document.querySelector(".fv__text");
+  const fvHeaderLogo = document.querySelector(".header__logo");
+  const fvAnniversaryLogo = document.querySelector(".fv__anniversary-logo");
+  const fvScrollLine = document.querySelector(".fv__scroll-line");
+  const fvImageWrapper = document.querySelector(".fv__image-wrapper");
+  const fvMessage = document.querySelector(".fv__message");
+  const fvCircleTextGray = document.querySelector(".fv__circle-text-gray");
+  const fvSmallCircleWrapper = document.querySelector(".fv__small-circle-wrapper");
+  const svgPath_1 = document.querySelector(".fv__bg-svg-path-1");
+  const svgPath_2 = document.querySelector(".fv__bg-svg-path-2");
+  const svgPath_3 = document.querySelector(".fv__bg-svg-path-3");
+  const svgPath_4 = document.querySelector(".fv__bg-svg-path-4");
   const easing = "linear";
   const pseudoElementProperty = "--border-opacity";
   let prefix;
@@ -492,7 +570,7 @@ function setSvgAnimation(windowWidth) {
     ease: easing,
     opacity: 1,
     onUpdate: () => {//（onUpdateの理由）timeline途中で画面幅resizeイベント起きた場合にも対応するため
-      setSmallCircleWrapperPosition();
+      setSmallCircleWrapperPosition(fvSmallCircleWrapper, svgPath_4);
     },
   }, "<");
 }
@@ -503,6 +581,7 @@ function setSvgAnimation(windowWidth) {
  * @param {Number} windowWidth ウィンドウ幅
  */
 function setSvgViewBoxSize(windowWidth) {
+  const fvBgSvg = document.querySelector(".fv__bg-svg");
   if (windowWidth >= pcBreakPoint) {
     fvBgSvg.setAttribute("viewBox", "0 0 1920 1080");
   } else {
@@ -512,9 +591,11 @@ function setSvgViewBoxSize(windowWidth) {
 
 /**
  * SmallCircleWrapperの位置設定
+ *
+ * @param {HTMLElement} fvSmallCircleWrapper
  */
-function setSmallCircleWrapperPosition() {
-  const pathData = svgPath_4.getBoundingClientRect();
+function setSmallCircleWrapperPosition(fvSmallCircleWrapper, targetSvgPath) {
+  const pathData = targetSvgPath.getBoundingClientRect();
   const width = pathData.width;
   const height = pathData.height;
   const top = pathData.top;
